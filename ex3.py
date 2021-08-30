5d25a14cfa85470e21fb6b368366d77cfb667896
benson955 <89002730+benson955@users.noreply.github.com>
2021-08-30 13:50:22 +0800

    added the previous parts of teh assignment

--- ex3.py
+++ ex3.py
@@ -1,8 +1,35 @@
-print("I will now count my homework:")
+print ("I will now count my chickens:")
+#performing 30 divided by 6 then adding 25 to sum
+print ("Hens", 25 + 30 / 6)
+#performing 100 minus (25 times 3 modulo 4)
+print ("Roosters", 100 -  25 * 3 % 4)
+print ("Now I will count the eggs:")
+#4 modulo 2 added and subtracted to the previous numbers, then 1 divided by 4 added 6 and subtracted to the previous part
+print (3 + 2 + 1 -  5 + 4 % 2 -  1 / 4 + 6)
 
+print ("Is it true that 3 + 2 < 5 -  7?")
+#printing a boolean of whether the inequality is true
+print (3 + 2 < 5 -  7)
+#printing a statement and a sum of 3 and 2
+print ("What is 3 + 2?", 3 + 2)
+#printing a statement and the difference of 5 and 7
+print ("What is 5 -  7?", 5 -  7)
+print ("Oh, that's why it's False.")
+print ("How about some more.")
+#a boolean of whether the inequality is true 
+print ("Is it greater?", 5 > - 2)
+#a boolean of whether the inequality / equality is true 
+print ("Is it greater or equal?", 5 >= - 2)
+#a boolean of whether the inequality / equality is true 
+print ("Is it less or equal?", 5 <= - 2)
+
+print("I will now count my homework:")
+#3 divided by 2 added to 1
 print("Projects", 1 + 3 / 2)
+#300 multiplied by 10 and divided in half
 print("Assignments", 300 * 10 / 2)
 
 print("Now counting the hours I need to spend:")
 
+#5 modulo 2 plus (5 times 100)
 print(5 % 2 + 5 * 100)
